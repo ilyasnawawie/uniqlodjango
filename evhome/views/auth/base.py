e8e23651898fc3beac25696d68bf5f2318bc3207
Ilyas <92800413+ilyasnawawie@users.noreply.github.com>
2023-06-06 17:05:06 +0800

    updated base for auth

--- evhome/views/auth/base.py
+++ evhome/views/auth/base.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.views import View
-from django.db.models import Q, ManyToOneRel
+from django.db.models import Q, ManyToOneRel, ForeignKey
 
 class ItemListView(View):
     model = None
@@ -32,7 +32,7 @@ class ItemListView(View):
 
         filter_conditions = Q()
         for field in fields:
-            if not isinstance(field, ManyToOneRel):
+            if not isinstance(field, (ManyToOneRel, ForeignKey)):
                 filter_conditions |= Q(**{f"{field.name}__icontains": query})
 
         items = self.model.objects.filter(filter_conditions)
